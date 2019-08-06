c0ab08ac0a2b8194814c21b3195e3b014bc0dcea
bryan0147 <bryan0147@gmail.com>
2019-08-06 10:25:55 -0400

    Create Model, Migration, and Controller files for Responses feature

---
+++ app/controllers/api/responses_controller.rb
@@ -0,0 +1,2 @@
+class Api::ResponsesController < ApplicationController
+end
---
+++ db/migrate/20190806140439_create_responses.rb
@@ -0,0 +1,12 @@
+class CreateResponses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :responses do |t|
+      t.text :string, null: false
+      t.integer :author_id, null: false
+      t.integer :story_id, null: false
+
+      t.timestamps
+    end
+    add_index [:author_id, :story_id], unique: true
+  end
+end
